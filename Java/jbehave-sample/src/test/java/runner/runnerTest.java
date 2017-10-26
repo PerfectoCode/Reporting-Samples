f1fc27b225d83230617f325f645a29b2af673e19
yuvals <yuvals@perfectomobile.com>
2017-10-26 17:26:07 +0300

    refactor to allow maven to run jbehave

--- Java/jbehave-sample/src/test/java/runner/runnerTest.java
+++ Java/jbehave-sample/src/test/java/runner/runnerTest.java
@@ -1,5 +1,6 @@
 package runner;
 
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -19,7 +20,7 @@ import org.jbehave.core.steps.Steps;
 import test.Customized;
 
 
-public class runner extends JUnitStories {
+public class runnerTest extends JUnitStories {
 
     DriverProvider driverProvider = new DriverProvider();
 
