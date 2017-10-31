5d63e33b46b4fd432337f361bd78df1310198634
yuvals <yuvals@perfectomobile.com>
2017-10-31 17:13:01 +0200

    capital

--- Java/jbehave-sample/src/test/java/runner/runnerTest.java
+++ Java/jbehave-sample/src/test/java/runner/runnerTest.java
@@ -20,7 +20,7 @@ import org.jbehave.core.steps.Steps;
 import test.CustomizedTest;
 
 
-public class runnerTest extends JUnitStories {
+public class RunnerTest extends JUnitStories {
 
     DriverProvider driverProvider = new DriverProvider();
 
