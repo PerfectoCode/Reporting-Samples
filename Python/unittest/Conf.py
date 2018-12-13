71983bfe039fa1099661c779eb762fce6b28749c
Michael Dikman <archmisha@users.noreply.github.com>
2018-12-13 09:38:46 +0200

    Update Conf.py

--- Python/unittest/Conf.py
+++ Python/unittest/Conf.py
@@ -1,6 +1,6 @@
 import unittest
 from selenium import webdriver
-from perfecto import PerfectoExecutionContext,TestResultFactory,TestContext,PerfectoReportiumClient
+from perfecto import PerfectoExecutionContext,TestResultFactory,TestContext,PerfectoReportiumClient,model
 
 
 class TestConf(unittest.TestCase):
@@ -22,22 +22,26 @@ class TestConf(unittest.TestCase):
         }
         self.driver = webdriver.Remote('https://' + self.host + '/nexperience/perfectomobile/wd/hub', capabilities)
         self.create_reporting_client()
-        cf2 = CustomField('key1', 'Tvalue1')
-        cf2 = CustomField('key2', 'Tvalue2')
+        cf2 = model.CustomField('key1', 'Tvalue1')
+        cf2 = model.CustomField('key2', 'Tvalue2')
         self.reporting_client.test_start(self.id(),
-                                         TestContext(cusFields=[cf1, cf2], test_tags=['Tag1', 'Tag2', 'Tag3']))
+                                         TestContext(customFields=[cf1, cf2], tags=['Tag1', 'Tag2', 'Tag3']))
  
     def run(self, result=None):
         self.currentResult = result  # remember result for use in tearDown
         unittest.TestCase.run(self, result)  # call superclass run method
 
     def tearDown(self):
+        cf1 = model.CustomField('key1', 'val1')
+        cf2 = model.CustomField('key2', 'val2')
+        tec = TestContext(customFields=[cf1, cf2], tags=['test1', 'test2'])
         try:
             if self.currentResult.wasSuccessful():
                 self.reporting_client.test_stop(TestResultFactory.create_success())
             else:
                 self.reporting_client.test_stop(TestResultFactory.create_failure(self.currentResult.errors,
-                                                                                 self.currentResult.failures))
+                                                                                 self.currentResult.failures),
+                                                                                 tec)
             # Print report's url
             print 'Report-Url: ' + self.reporting_client.report_url() + '\n'
         except Exception as e:
@@ -46,13 +50,13 @@ class TestConf(unittest.TestCase):
         self.driver.quit()
 
     def create_reporting_client(self):
-        cf1 = CustomField('key1','Evalue1')
-        cf2 = CustomField('key3', 'Evalue3'])
+        cf1 = model.CustomField('key1','Evalue1')
+        cf2 = model.CustomField('key3', 'Evalue3'])
 
         perfecto_execution_context = PerfectoExecutionContext(webdriver=self.driver,
-                                                              context_tags=['Etag0', 'Etag1', 'Etag2', 'Etag3'],
-                                                              job=Job('Jobname', '12', 'branch_name'),
-                                                              project=Project('project_name', '2.0'),
-                                                              cusFields=[cf1, cf2])
+                                                              tags=['Etag0', 'Etag1', 'Etag2', 'Etag3'],
+                                                              job=model.Job('Jobname', 12, 'branch_name'),
+                                                              project=model.Project('project_name', 2.0),
+                                                              customFields=[cf1, cf2])
         
         self.reporting_client = PerfectoReportiumClient(perfecto_execution_context)
