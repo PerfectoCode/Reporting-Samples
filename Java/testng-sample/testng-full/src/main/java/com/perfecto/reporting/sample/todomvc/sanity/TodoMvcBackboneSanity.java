6cbdf25f4396203a21d69c35dd66c13301db14c2
yuvalShalgi227 <yuvals@perfectomobile.com>
2019-03-03 15:52:39 +0200

    remove allure fin

--- Java/testng-sample/testng-full/src/main/java/com/perfecto/reporting/sample/todomvc/sanity/TodoMvcBackboneSanity.java
+++ Java/testng-sample/testng-full/src/main/java/com/perfecto/reporting/sample/todomvc/sanity/TodoMvcBackboneSanity.java
@@ -4,11 +4,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import ru.yandex.qatools.allure.annotations.Step;
 
-/**
- * Demonstrate Perfecto's reporting solution with Allure's @Step annotation
- */
+
+
 public class TodoMvcBackboneSanity extends AbstractTodoMvcSanityTest {
 
     @Override
@@ -42,13 +40,13 @@ public class TodoMvcBackboneSanity extends AbstractTodoMvcSanityTest {
     /**
      * Click on the "Completed" button to show only completed todos
      */
-    @Step("Filter active todos")
+
     protected void filterActive() {
         WebElement active = findElement(By.xpath("//a[@href='#/active']"));
         active.click();
     }
 
-    @Step("Check that title equals 'Backbone.js • TodoMVC'")
+
     protected void verifyTitle() {
         Assert.assertEquals(driver.getTitle(), "Backbone.js • TodoMVC");
     }
