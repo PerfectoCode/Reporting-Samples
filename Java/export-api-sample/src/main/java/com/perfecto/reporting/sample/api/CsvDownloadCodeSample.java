8222aca6db38e7e795b5c54840c29d83e2688500
Elisheva Klein <elishevak1@users.noreply.github.com>
2023-01-24 12:58:02 +0200

    Update CsvDownloadCodeSample.java

--- Java/export-api-sample/src/main/java/com/perfecto/reporting/sample/api/CsvDownloadCodeSample.java
+++ Java/export-api-sample/src/main/java/com/perfecto/reporting/sample/api/CsvDownloadCodeSample.java
@@ -1,8 +1,5 @@
 package com.perfecto.reporting.sample.api;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
-
 import java.awt.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -22,24 +19,12 @@ public class CsvDownloadCodeSample {
 
         // Download a CSV report - create a "task" for CSV generation and download the CSV on task completion
         Path testCsvPath = Paths.get(tempDir.toString(), csvFileName + ".csv");
-        ReportiumExportUtils.downloadCsvTestReport(testCsvPath,createRequestBody());
+        ReportiumExportUtils.downloadCsvReport(testCsvPath, ReportiumExportUtils.createRequestBody());
 
         try {
             Desktop.getDesktop().open(tempDir.toFile());
         } catch (Exception e) {
             System.out.println("Data was saved in: " + tempDir.toString());
         }
-    }
-
-    private static JsonObject createRequestBody() {
-        JsonObject jsonBody = new JsonObject();
-        JsonObject filerJson = new JsonObject();
-        JsonObject fieldsJson = new JsonObject();
-        JsonArray values = new JsonArray();
-        values.add(System.currentTimeMillis() - 24 * 60 * 60 * 1000);
-        fieldsJson.add("startExecutionTime", values);
-        filerJson.add("fields", fieldsJson);
-        jsonBody.add("filter", filerJson);
-        return jsonBody;
     }
 }
