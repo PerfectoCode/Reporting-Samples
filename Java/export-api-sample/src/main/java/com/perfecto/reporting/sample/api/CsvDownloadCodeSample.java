fbb4e0d54c40bfa6f40a910e82291cf446c53f3b
Elisheva Klein <eklein@perforce.com>
2023-08-18 01:13:48 +0300

    MOB-23501: Fix code sample to get CSV file by execution Id

--- Java/export-api-sample/src/main/java/com/perfecto/reporting/sample/api/CsvDownloadCodeSample.java
+++ Java/export-api-sample/src/main/java/com/perfecto/reporting/sample/api/CsvDownloadCodeSample.java
@@ -18,8 +18,16 @@ public class CsvDownloadCodeSample {
         String csvFileName = "Report_Library_Sample";
 
         // Download a CSV report - create a "task" for CSV generation and download the CSV on task completion
-        Path testCsvPath = Paths.get(tempDir.toString(), csvFileName + ".csv");
-        ReportiumExportUtils.downloadCsvReport(testCsvPath, ReportiumExportUtils.createRequestBody());
+        Path testsCsvPath = Paths.get(tempDir.toString(), csvFileName + ".csv");
+        // To get data about all the executions from the last 24 hours:
+        ReportiumExportUtils.downloadCsvReport(testsCsvPath, ReportiumExportUtils.createRequestBody());
+
+        // To get csv file for particular execution:
+        // You can get executionId value from your driver like:
+        // String executionId = (String) driver.getCapabilities().getCapability("executionId");
+        String executionId = "executionId";
+        Path testCsvPath = Paths.get(tempDir.toString(), csvFileName + "_" + executionId + ".csv");
+        ReportiumExportUtils.downloadCsvReport(testCsvPath, ReportiumExportUtils.createRequestBody(executionId));
 
         try {
             Desktop.getDesktop().open(tempDir.toFile());
